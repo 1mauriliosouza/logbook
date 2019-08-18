53064640bd6237b67ce1e2ad6ac40b8039472cc5
1mauriliosouza <contact@1mauriliosouza.pt>
2019-08-18 13:11:35 +0100

    Update 18-AUG-2019

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,7 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Digite o número do ticket"></b-form-input>
+          <b-form-input size="sm" class="mr-sm-2" type="text" v-model="search" placeholder="Digite o número do ticket"></b-form-input>
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Pesquisar</b-button>
         </b-nav-form>
 
@@ -41,6 +41,11 @@ export default {
     name: "Navbar",
     props: {
         msg: String
+    },
+    data(){
+        return{
+            
+        }
     }
 }
 </script>
